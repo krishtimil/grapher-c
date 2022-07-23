c0afe36f55f31203104d401e8fbd0588f78ed68a
krishtimil <30183049+krishtimil@users.noreply.github.com>
2022-07-23 14:47:14 +0545

    Defined a structure

---
+++ src/equation.h
@@ -0,0 +1,11 @@
+#ifndef UI_H
+#define UI_H
+#include "raylib.h"
+
+typedef struct Equation{
+    char *label;
+    Color col;
+    char *type;
+} Equation;
+
+#endif
--- src/ui.c
+++ src/ui.c
@@ -2,8 +2,10 @@
 
 #define RAYGUI_IMPLEMENTATION
 #include "raygui.h" // Required for GUI controls
-
+#include "equation.h"
 #include "graph.h"
+
+Equation equation_arr[8];
 
 //------------------------------------------------------------------------------------
 // Program main entry point
@@ -19,14 +21,15 @@ int init_ui(void)
     SetTargetFPS(60); // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
     int i = 0;
+
+    // For testing purposes
+    equation_arr[0].label = "mx+y=z";
+    equation_arr[0].col = GREEN;
+
     // Main game loop
     while (!WindowShouldClose()) // Detect window close button or ESC key
     {
-        // Update
-        //----------------------------------------------------------------------------------
-        // NOTE: All variables update happens inside GUI control functions
-        //----------------------------------------------------------------------------------
-        
+
         // Draw
         //----------------------------------------------------------------------------------
         BeginDrawing();
@@ -37,16 +40,17 @@ int init_ui(void)
         DrawRectangle(0, 0, GetScreenWidth() - 600, GetScreenHeight(), Fade(LIGHTGRAY, 0.3f));
         draw_graph();
         show_eq();
-    
-        for(int j=0; j<i; j++)
+
+        for (int j = 0; j < i; j++)
             box_eq(j);
 
-        if(i<8 && GuiButton((Rectangle) {15, i*70 + 20, 270, 50}, "Add Equation")){
-             i++;
+        if (i < 8 && GuiButton((Rectangle){15, i * 70 + 20, 270, 50}, "Add Equation"))
+        {
+            i++;
         }
         // if (drawRing) DrawRing(center, innerRadius, outerRadius, startAngle, endAngle, segments, Fade(MAROON, 0.3f));
 
-        DrawFPS(GetScreenWidth() - 100, 10); //Displays FPS on Screen
+        DrawFPS(GetScreenWidth() - 100, 10); // Displays FPS on Screen
 
         EndDrawing();
         //----------------------------------------------------------------------------------
@@ -71,8 +75,7 @@ int box_eq(int i)
 
     // Draw GUI controls
     //------------------------------------------------------------------------------
-    showEq = GuiCheckBox((Rectangle){15, i*70 + 15 + 20, 20, 20}, "\0", showEq);
-    DrawRectangleLines(50, i*70 + 20, 235, 50, BLACK);
-    DrawText("y=mx+c", 50 + 10, i*70 + 20 + 10, 30, GREEN);
+    showEq = GuiCheckBox((Rectangle){15, i * 70 + 15 + 20, 20, 20}, "\0", showEq);
+    DrawRectangleLines(50, i * 70 + 20, 235, 50, BLACK);
+    DrawText(equation_arr[i].label, 50 + 10, i * 70 + 20 + 10, 30, GREEN);
 }
-
